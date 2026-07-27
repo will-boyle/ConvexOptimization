a4a959871c148b7093719f8fa31705e426f2c6cb
Will Boyle <134346051+will-boyle@users.noreply.github.com>
2026-07-27 18:38:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sympy as sp
 from solver  import parse, phase1, solve
 from solver2 import solve2
 from solver3 import solve3
-from solver4 import parse_dc, solve_dc
+from dc_solver import parse_dc, solve_dc
 from c2_solver import parse_abb, solve_abb
 from lipschitz_solver import parse_lipschitz, solve_lipschitz, estimate_L
 
